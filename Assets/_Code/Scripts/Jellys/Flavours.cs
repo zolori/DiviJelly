024a277458632331c60d45d8a57a213922a19f2b
Tom Saury <saurytom@gmail.com>
2024-04-18 10:23:42 +0200

    Basic jelly controller

---
+++ Assets/_Code/Scripts/Jellys/Flavours.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Flavour
+{
+	Strawberry,
+	Orange,
+	Lemon,
+	Apple,
+	Blueberry,
+}
+
+[Serializable]
+public struct FlavourData
+{
+	public Flavour Flavour;
+	public Sprite Sprite;
+	public int Layer;
+}
+
+[CreateAssetMenu(fileName = "Flavours", menuName = "Flavour/Flavours", order = 1)]
+public class Flavours : ScriptableObject
+{
+	public List<FlavourData> Data;
+}
