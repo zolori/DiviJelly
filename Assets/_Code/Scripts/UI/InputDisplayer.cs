b7dada4cd5f05832633d9dcde6cd55003419ed83
Tom Saury <saurytom@gmail.com>
2024-05-29 10:02:03 +0200

    Input displayer now displays img

--- Assets/_Code/Scripts/UI/InputDisplayer.cs
+++ Assets/_Code/Scripts/UI/InputDisplayer.cs
@@ -1,13 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class InputDisplayer : MonoBehaviour
 {
+	[SerializeField] private InputsSprites m_InputsSprites;
 	[SerializeField] private string m_ActionName;
 	[SerializeField] private TextMeshProUGUI m_TextDisplay;
+	[SerializeField] private Image m_ImgDisplay;
 
 	InputMaster m_InputMaster;
 
@@ -24,5 +28,20 @@ public class InputDisplayer : MonoBehaviour
 		InputAction inputAction = m_InputMaster.InputAction.FindAction(m_ActionName);
 		InputBinding bindingMask = InputBinding.MaskByGroup(m_InputMaster.ControlScheme.bindingGroup);
 		m_TextDisplay.text = inputAction.GetBindingDisplayString(bindingMask);
+
+		string keyName = inputAction.GetBindingDisplayString(bindingMask, InputBinding.DisplayStringOptions.DontOmitDevice);
+		Debug.Log(keyName);
+		m_ImgDisplay.sprite = _FindInputSprite(keyName);
+	}
+
+	private Sprite _FindInputSprite(string iKeyName)
+	{
+		foreach(InputsSprites.ImageInputsPair inputsSprite in m_InputsSprites.InputsImages)
+		{
+			if(inputsSprite.Inputs.Contains(iKeyName))
+				return inputsSprite.Img;
+		}
+
+		return m_InputsSprites.DefaultInputImg;
 	}
 }
